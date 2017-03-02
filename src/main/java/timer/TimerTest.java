a90df715929c4a3de3fa221c30ad5dcd0e028afd
goni9071 <goni9071@hisco.co.kr>
2017-03-02 11:18:48 +0900

    Timer schedule 테스트

---
+++ src/main/java/timer/TimerTest.java
@@ -0,0 +1,26 @@
+package timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class TimerTest {
+  public static void main(String[] args) {
+    Timer timer = new Timer("Test");
+
+    TimerTask timerTask = new TimerTask() {
+      public void run() {
+        try {
+          TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        System.out.println(System.currentTimeMillis());
+      }
+    };
+
+    timer.schedule(timerTask, 0, 1000 * 1); // 1 초
+  }
+
+}
