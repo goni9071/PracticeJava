ce80d6d1c100170fe34c7adaa648f8a52990a49a
goni9071 <goni9071@hisco.co.kr>
2017-03-02 16:48:03 +0900

    string right pad 잘못쓰면. 망.

--- src/main/java/string/Format.java
+++ src/main/java/string/Format.java
@@ -16,7 +16,8 @@ public class Format {
     System.out.println(String.format("%02X", bytes[1]));
     System.out.println(String.format("%02X", bytes[2]));
     System.out.println(String.format("%-10s", a) + "물");// rpad 공백
-    System.out.println(String.format("%-3s", "가나다라") + "마");// rpad 숫자보다 많으면?
+    System.out.println(String.format("%1$-3s", "가나다라") + "마");// rpad 숫자보다 많으면?
+    System.out.println(String.format("%1$-10s", "가나 다라").replace(" ", "0"));// rpad 0
     String.format("%,d", 1000000);
   }
 }
